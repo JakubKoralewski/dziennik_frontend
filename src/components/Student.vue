967eb3e4b19312bac809043774cb6200bf382e36
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-03-03 16:08:04 +0100

    Student.vue move each student to own component

---
+++ src/components/Student.vue
@@ -0,0 +1,140 @@
+<template>
+	<div class="student">
+		<div id="header">
+			<div id="name">{{ student.imie }} {{ student.nazwisko }}</div>
+			<div id="tools">
+				<i class="fas fa-edit"></i>
+				<i class="fas fa-trash-alt"></i>
+			</div>
+		</div>
+		<div id="content">
+			<div id="klasa">
+				Klasa:
+				<span class="info">{{student.klasa}}</span>
+			</div>
+			<div id="telefon">
+				Telefon:
+				<span class="info">{{student.telefon}}</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import { IStudent } from '@/interfaces';
+
+	export default Vue.extend({
+		name: 'Student',
+		props: {
+			student: {
+				type: Object,
+				required: true,
+			},
+		},
+	});
+</script>
+
+<style scoped lang="scss">
+	@import '@/scss/_mixins.scss';
+	.student {
+		overflow: hidden;
+		/* width: 25%; */
+		// height: 25%; // makes elements unnaturally shrink when height very small
+
+		// flex-basis: 25%;
+		max-width: calc(61.8% + 6.18rem); // golden ratio
+
+		border-radius: 1rem;
+		$student-bg-color: lighten($main-color, 2%);
+		background-color: $student-bg-color;
+		flex-grow: 1;
+		margin-right: $students-left-padding;
+		margin-bottom: $students-top-padding;
+		@include nav-shadow;
+		display: flex;
+		flex-direction: column;
+		transition: opacity 0.25s ease-in-out;
+
+		&.invisible {
+			opacity: 0;
+			// display: none;
+		}
+
+		#content {
+			box-sizing: border-box;
+			padding: 1rem;
+			font-weight: 300;
+
+			.info {
+				font-weight: 400;
+			}
+		}
+
+		#header {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			width: auto;
+			padding: calc(0.5rem + 1vh) calc(0.5rem + 1vw);
+			background-color: darken($student-bg-color, 2%);
+			// background-color: lightblue;
+			// border-bottom-right-radius: 0.5rem;
+			// border-bottom-left-radius: 0.5rem;
+			border-style: solid;
+			border-width: 0px;
+			border-bottom-width: 1px;
+			border-color: transparentize(black, 0.9);
+
+			// height: calc(1rem + 1vh);
+			// height: 50rem;
+
+			#name {
+				text-align: center;
+				line-height: 100%;
+				height: 100%;
+				font-weight: 500;
+				word-break: break-word;
+			}
+
+			#tools {
+				margin-left: auto;
+				// background-color: darken($main-color, 2%);
+				padding: 0.1rem;
+				border-radius: 0.1rem;
+				i {
+					margin-left: 1vw;
+					cursor: pointer;
+					transition: all 0.25s ease-in-out;
+					transition-property: color, transform;
+					transform-origin: center center;
+
+					&:hover {
+						animation-name: wiggle;
+						animation-duration: 1000ms;
+						animation-iteration-count: 1;
+						animation-timing-function: ease-out;
+						&.fa-edit {
+							color: lighten(blue, 20%);
+						}
+						&.fa-trash-alt {
+							color: lighten(red, 20%);
+						}
+					}
+
+					&:active {
+						transition: font-size 100ms ease-in;
+						font-size: 1.1rem;
+					}
+				}
+			}
+		}
+
+		// &:nth-child(even) {
+		// 	background-color: darken($main-color, 1%);
+		// }
+		// &:nth-child(odd) {
+		// 	background-color: lighten($main-color, 1%);
+		// }
+	}
+</style>
--- src/views/Authorized.vue
+++ src/views/Authorized.vue
@@ -5,30 +5,12 @@
 		<div id="content">
 			<NavTitle id="nav-title"/>
 			<div id="students" v-if="students">
-				<div
-					class="student"
+				<Student
 					v-for="student of students"
 					:key="student.id"
 					:class="{'invisible': !student.visible}"
-				>
-					<div id="header">
-						<div id="name">{{ student.imie }} {{ student.nazwisko }}</div>
-						<div id="tools">
-							<i class="fas fa-edit"></i>
-							<i class="fas fa-trash-alt"></i>
-						</div>
-					</div>
-					<div id="content">
-						<div id="klasa">
-							Klasa:
-							<span class="info">{{student.klasa}}</span>
-						</div>
-						<div id="telefon">
-							Telefon:
-							<span class="info">{{student.telefon}}</span>
-						</div>
-					</div>
-				</div>
+					:student="student"
+				/>
 			</div>
 		</div>
 	</div>
@@ -38,6 +20,7 @@
 	import Vue from 'vue';
 	import NavBar from '@/components/NavBar.vue';
 	import NavTitle from '@/components/NavTitle.vue';
+	import Student from '@/components/Student.vue';
 	import AddButton from '@/components/AddButton.vue';
 
 	import { mapGetters, mapActions, mapState, Computed } from 'vuex';
@@ -67,6 +50,7 @@
 			NavBar,
 			NavTitle,
 			AddButton,
+			Student,
 		},
 		computed: {
 			...mapState(['students']),
@@ -101,12 +85,7 @@
 </style>
 
 <style lang="scss" scoped>
-	@mixin nav-shadow($size: 1px) {
-		box-shadow: $size $size 72px -14px rgba(0, 0, 0, 0.1);
-	}
-	@mixin nav-border($size) {
-		border: $size solid darken($main-color, 20%);
-	}
+	@import '@/scss/_mixins.scss';
 
 	@import '@/scss/wiggle.scss';
 
@@ -123,9 +102,7 @@
 			height: 100%;
 			width: 100%;
 			#students {
-				$left-padding: 3rem;
-				$top-padding: 2rem;
-				padding: $top-padding 0rem 0rem $left-padding;
+				padding: $students-top-padding 0rem 0rem $students-left-padding;
 				// height: 100%; // makes white footer
 				display: flex;
 				justify-content: flex-start;
@@ -135,107 +112,6 @@
 
 				flex-direction: row;
 				flex-wrap: wrap;
-
-				.student {
-					overflow: hidden;
-					/* width: 25%; */
-					// height: 25%; // makes elements unnaturally shrink when height very small
-
-					// flex-basis: 25%;
-					max-width: calc(61.8% + 6.18rem); // golden ratio
-
-					border-radius: 1rem;
-					$student-bg-color: lighten($main-color, 2%);
-					background-color: $student-bg-color;
-					flex-grow: 1;
-					margin-right: $left-padding;
-					margin-bottom: $top-padding;
-					@include nav-shadow;
-					display: flex;
-					flex-direction: column;
-					transition: opacity 0.25s ease-in-out;
-
-					&.invisible {
-						opacity: 0;
-						// display: none;
-					}
-
-					#content {
-						box-sizing: border-box;
-						padding: 1rem;
-						font-weight: 300;
-
-						.info {
-							font-weight: 400;
-						}
-					}
-
-					#header {
-						display: flex;
-						flex-direction: row;
-						align-items: center;
-						width: auto;
-						padding: calc(0.5rem + 1vh) calc(0.5rem + 1vw);
-						background-color: darken($student-bg-color, 2%);
-						// background-color: lightblue;
-						// border-bottom-right-radius: 0.5rem;
-						// border-bottom-left-radius: 0.5rem;
-						border-style: solid;
-						border-width: 0px;
-						border-bottom-width: 1px;
-						border-color: transparentize(black, 0.9);
-
-						// height: calc(1rem + 1vh);
-						// height: 50rem;
-
-						#name {
-							text-align: center;
-							line-height: 100%;
-							height: 100%;
-							font-weight: 500;
-							word-break: break-word;
-						}
-
-						#tools {
-							margin-left: auto;
-							// background-color: darken($main-color, 2%);
-							padding: 0.1rem;
-							border-radius: 0.1rem;
-							i {
-								margin-left: 1vw;
-								cursor: pointer;
-								transition: all 0.25s ease-in-out;
-								transition-property: color, transform;
-								transform-origin: center center;
-
-								&:hover {
-									animation-name: wiggle;
-									animation-duration: 1000ms;
-									animation-iteration-count: 1;
-									animation-timing-function: ease-out;
-									&.fa-edit {
-										color: lighten(blue, 20%);
-									}
-									&.fa-trash-alt {
-										color: lighten(red, 20%);
-									}
-								}
-
-								&:active {
-									transition: font-size 100ms ease-in;
-									font-size: 1.1rem;
-								}
-							}
-						}
-					}
-
-					// &:nth-child(even) {
-					// 	background-color: darken($main-color, 1%);
-					// }
-					// &:nth-child(odd) {
-					// 	background-color: lighten($main-color, 1%);
-					// }
-				}
 			}
 		}
 
